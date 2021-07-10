c8235f523d5888feb75df225c4253c29463061f9
Himakuma(kumatta_ss) <ssapp.devuser@gmail.com>
2021-07-10 18:00:04 +0900

    UnityとMagicOnionのAndroid・iOS動作確認編

---
+++ KumattaApp-Client/Assets/KumattaApp/Editor/KumattaAppBuild.cs
@@ -0,0 +1,50 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class KumattaAppBuild : IPostprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    [PostProcessBuild(1)]
+    public static void OnPostProcessBuild(BuildTarget target, string path)
+    {
+        if (target == BuildTarget.iOS)
+        {
+            IOSPostProcessBuild(path);
+        }
+    }
+
+    private static void IOSPostProcessBuild(string path)
+    {
+        var projectFilePath = PBXProject.GetPBXProjectPath(path);
+        var project = new PBXProject();
+        project.ReadFromFile(projectFilePath);
+
+        var targetGuid = project.GetUnityFrameworkTargetGuid();
+        project.AddFrameworkToProject(targetGuid, "libz.tbd", false);
+
+        project.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
+        project.WriteToFile(projectFilePath);
+    }
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        if (report.summary.platform == BuildTarget.iOS)
+        {
+            IOSOnPostprocessBuild(report);
+        }
+    }
+
+    public void IOSOnPostprocessBuild(BuildReport report)
+    {
+        string plistPath = Path.Combine(report.summary.outputPath, "Info.plist");
+        PlistDocument plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+        plist.root.SetString("NSLocalNetworkUsageDescription", "テストの為");
+        plist.WriteToFile(plistPath);
+    }
+}
--- KumattaApp-Client/Assets/KumattaApp/Scripts/MyApp/KumattaAppCall.cs
+++ KumattaApp-Client/Assets/KumattaApp/Scripts/MyApp/KumattaAppCall.cs
@@ -2,24 +2,27 @@ using UnityEngine;
 using Grpc.Core;
 using MagicOnion.Client;
 using MyApp.Shared;
+using TMPro;
 
 public class KumattaAppCall : MonoBehaviour
 {
+
     void Start()
     {
         CallTest();
     }
 
-    public async void CallTest() 
+    public async void CallTest()
     {
-        string host = "localhost";
+        string host = "ローカルのIP";
         int port = 5001;
         var channel = new Channel(host, port, ChannelCredentials.Insecure);
         //var channel = new Channel(host, port, new SslCredentials());
 
         var client = MagicOnionClient.Create<IMyFirstService>(channel);
         var result = await client.SumAsync(100, 23);
-        Debug.Log($"Result: {result}");
-    }
 
+        var textUI = FindObjectOfType<TextMeshProUGUI>();
+        textUI.text = $"Result: {result}";
+    }
 }
